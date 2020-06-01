befe297f8425ec7e1fd57aeff025268c60aec2c4
Ben Mallonee <ben.mallonee.c@gmai.com>
2020-06-01 12:22:58 -0700

    Game Manager Created

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public int score;
+
+    public GameObject player;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+            Debug.LogError("[GameManager] Attempted to create a second game manager." + this.gameObject.name);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
