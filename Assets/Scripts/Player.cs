07e0fbf0f023439b2ab48ce156322458397dae29
Ben Mallonee <ben.mallonee.c@gmai.com>
2020-06-03 12:09:58 -0700

    Player Movement Added

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Transform tf;
+    public float turnSpeed = 175f;
+    public float moveSpeed = 5f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +17,28 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            tf.Rotate(0, 0, turnSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            tf.Rotate(0, 0, -turnSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            //tf.Translate(Vector3.right * moveSpeed * Time.deltaTime, Space.Self);
+            tf.position += tf.right * moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Shoot();
+        }
+    }
+
+    public void Shoot()
+    {
+        //ToDo: Implement Shooting
+        Debug.Log("Shootng not implemented yet.");
     }
 }
